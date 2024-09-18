fa0f840df52942d85cde7e1757e20cd2985449c1
Rishabh Gupta <belikerishabh@gmail.com>
2024-09-19 00:09:50 +0530

    feat: convert dsn to circuit json

--- lib/dsn-pcb/index.ts
+++ lib/dsn-pcb/index.ts
@@ -1,1 +1,2 @@
 export * from "./types.ts";
+export * from "./dsn-pcb-to-circuit-json.ts";
--- tests/dsn-pcb/parse-dsn-pcb.test.ts
+++ tests/dsn-pcb/parse-dsn-pcb.test.ts
@@ -1,11 +1,20 @@
-import { expect, test } from "bun:test";
+import { circuitJsonToPcbSvg } from "circuit-to-svg";
 import { parseDSN } from "../../lib/common/parse-sexpr.ts";
-// @ts-ignore
+import { dsnPcbToCircuitJson } from "../../lib/dsn-pcb/dsn-pcb-to-circuit-json.ts";
 import testDsnFile from "../assets/testkicadproject/testkicadproject.dsn" with {
   type: "text",
 };
+// @ts-ignore
+import { expect, test } from "bun:test";
 
-test("parseDSN", async () => {
-  const sexpr = parseDSN(testDsnFile);
-  expect(sexpr).toBeTruthy();
+test("parse s-expr to json", async () => {
+  const pcbJson = parseDSN(testDsnFile);
+  expect(pcbJson).toBeTruthy();
+});
+
+test("parse json to circuit json", async () => {
+  const pcb = parseDSN(testDsnFile);
+  const circuitJson = dsnPcbToCircuitJson(pcb);
+
+  expect(circuitJsonToPcbSvg(circuitJson)).toMatchSvgSnapshot(import.meta.path);
 });
