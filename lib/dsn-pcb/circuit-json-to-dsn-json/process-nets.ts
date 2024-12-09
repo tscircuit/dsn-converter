644f9bfbf88897889081e52fe38160766d2506d8
Rishabh Gupta <belikerishabh@gmail.com>
2024-12-09 16:50:27 +0530

    fix: get the unconnected pads as well in nets list

--- lib/dsn-pcb/circuit-json-to-dsn-json/process-nets.ts
+++ lib/dsn-pcb/circuit-json-to-dsn-json/process-nets.ts
@@ -31,8 +31,9 @@ export function processNets(circuitElements: AnyCircuitElement[], pcb: DsnPcb) {
           const componentName =
             componentNameMap.get(sourcePort.source_component_id) || ""
           // "A12" -> "12", "B1" -> "1 (When pinNumber a)
-          const pinNumber =
-            element.port_hints?.[0]?.match(/(?:A|B)?(\d+)/)?.[1] || "1"
+          const pinNumber = sourcePort.port_hints?.find(
+            (hint) => !Number.isNaN(Number(hint)),
+          )
 
           padsBySourcePortId.set(sourcePort.source_port_id, {
             componentName,
