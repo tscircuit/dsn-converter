74fa62e4934efe3d842f3b2272c972c9c2a4259e
tscircuitbot <tscircuitbot@users.noreply.github.com>
2024-12-14 16:13:21 +0000

    formatbot: Automatically format code

--- lib/dsn-pcb/dsn-json-to-circuit-json/convert-dsn-session-to-circuit-json.ts
+++ lib/dsn-pcb/dsn-json-to-circuit-json/convert-dsn-session-to-circuit-json.ts
@@ -121,7 +121,7 @@ export function convertDsnSessionToCircuitJson(
             fromLayer,
             toLayer,
           }),
-        })       
+        })
         sessionElements.push(...routeSegments)
       })
     }
