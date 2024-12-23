846e2e9572b65fe5f118f34b8a6531a6001c63e0
Rishabh Gupta <belikerishabh@gmail.com>
2024-12-23 18:03:52 +0530

    fix: hover over the traces or ports with proper linkage

---
+++ tests/repros/repro-7-fixing-hover.test.ts
@@ -0,0 +1,46 @@
+import { expect, test } from "bun:test"
+import {
+  convertCircuitJsonToDsnJson,
+  convertDsnSessionToCircuitJson,
+  parseDsnToDsnJson,
+} from "lib"
+
+import circuitJson from "../assets/repro/motor-driver-breakout-circuit.json"
+// @ts-ignore
+import outputSesFile from "../assets/repro/output_motor.ses" with {
+  type: "text",
+}
+
+import type { AnyCircuitElement } from "circuit-json"
+import type { DsnSession } from "lib"
+import { su } from "@tscircuit/soup-util"
+
+test("motor driver circuit with hover", async () => {
+  const dsnPcb = convertCircuitJsonToDsnJson(circuitJson as AnyCircuitElement[])
+  const sessionJson = parseDsnToDsnJson(outputSesFile) as DsnSession
+
+  const circuitJsonFromSession = convertDsnSessionToCircuitJson(
+    dsnPcb,
+    sessionJson,
+    circuitJson as AnyCircuitElement[],
+  )
+
+  const outputPcbTraces = circuitJsonFromSession.filter(
+    (element) => element.type === "pcb_trace",
+  )
+
+  const newCircuitJson = [...circuitJson, ...outputPcbTraces]
+
+  const pcbTraces = su(newCircuitJson as AnyCircuitElement[]).pcb_trace.list()
+  const sourceTraceIds = pcbTraces.map((trace) => parseInt(trace.source_trace_id?.replace("source_trace_", "") ?? "0"))
+
+  expect(pcbTraces.length).toBe(67)
+  expect(sourceTraceIds.every((id) => id >= 0 && id <= 22)).toBe(true)
+
+  // Checking the proper linkage of source traces to source ports
+  expect(sourceTraceIds.filter((id) => id === 17).length).toBe(2)
+  expect(sourceTraceIds.filter((id) => id === 5).length).toBe(21)
+  expect(sourceTraceIds.filter((id) => id === 4).length).toBe(5)
+  expect(sourceTraceIds.filter((id) => id === 10).length).toBe(6)
+  expect(sourceTraceIds.filter((id) => id === 0).length).toBe(11)
+})
