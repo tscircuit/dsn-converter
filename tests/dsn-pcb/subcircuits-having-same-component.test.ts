8273581cff9f57b1325184e5e51e761792190ee8
Rishabh Gupta <belikerishabh@gmail.com>
2025-01-15 11:58:24 +0530

    test update

--- tests/dsn-pcb/subcircuits-having-same-component.test.ts
+++ tests/dsn-pcb/subcircuits-having-same-component.test.ts
@@ -1,11 +1,16 @@
 import { expect, test } from "bun:test"
-import { convertCircuitJsonToDsnString, type DsnPcb } from "lib"
+import { convertCircuitJsonToDsnString, convertCircuitJsonToDsnJson } from "lib"
 import inputCircuitFile from "../assets/repro/subcircuits-same-components.json"
 import type { AnyCircuitElement } from "circuit-json"
 
 test("convert circuit json to dsn pcb", async () => {
   const circuitJson = inputCircuitFile as AnyCircuitElement[]
-  const dsnPcb = convertCircuitJsonToDsnString(circuitJson)
+  const dsnJson = convertCircuitJsonToDsnJson(circuitJson)
 
-  await Bun.write("output.dsn", dsnPcb)
+  expect(dsnJson.network.nets.length).toBe(6)
+
+  const net1Count = dsnJson.network.nets.filter((net) =>
+    net.name.startsWith("NET1"),
+  ).length
+  expect(net1Count).toBe(2)
 })
