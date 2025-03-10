430804cfa17ef64e0bd357b55e86558c98aafb1c
Rishabh Gupta <belikerishabh@gmail.com>
2025-03-11 00:21:39 +0530

    info for conversion unit

--- lib/dsn-pcb/dsn-json-to-circuit-json/dsn-component-converters/convert-wiring-path-to-pcb-traces.ts
+++ lib/dsn-pcb/dsn-json-to-circuit-json/dsn-component-converters/convert-wiring-path-to-pcb-traces.ts
@@ -40,7 +40,7 @@ export const convertWiringPathToPcbTraces = ({
       route_type: "wire" as const,
       x: Number(point.x.toFixed(4)),
       y: Number(point.y.toFixed(4)),
-      width: wire.path!.width / 10000,
+      width: wire.path!.width / 10000, // um to mm
       layer: wire.path!.layer.includes("F.") ? "top" : "bottom",
     }))
 
