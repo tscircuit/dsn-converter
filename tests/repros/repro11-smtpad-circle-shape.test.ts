b525280db5198d90109ea7de6b9f11c461a0539c
Rishabh Gupta <belikerishabh@gmail.com>
2025-03-30 17:04:26 +0530

    fix: radius to diameter coversion

--- tests/repros/repro11-smtpad-circle-shape.test.ts
+++ tests/repros/repro11-smtpad-circle-shape.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "bun:test"
-import { convertCircuitJsonToDsnJson } from "lib"
+import { convertCircuitJsonToDsnJson, convertCircuitJsonToDsnString } from "lib"
 
 import circuitJson from "../assets/repro/XIAO_S3.json"
 import type { AnyCircuitElement } from "circuit-json"
@@ -10,11 +10,11 @@ test("smtpad shape circle have correct padstack name", async () => {
   )
 
   const padstackName = dsnJson.library.padstacks.filter(
-    (padstack) => padstack.name === "Round[T]Pad_800_800_um",
+    (padstack) => padstack.name === "Round[T]Pad_1600_1600_um",
   )
   expect(padstackName).toBeDefined()
   expect(padstackName?.length).toBe(1)
   expect(padstackName?.[0].shapes.length).toBe(2)
   expect(padstackName?.[0].hole?.shape).toBe("circle")
-  expect(padstackName?.[0].hole?.diameter).toBe(800)
+  expect(padstackName?.[0].hole?.diameter).toBe(1600)
 })
