011372751f7180850079fd13066e52d5a491c04e
Rushabh Patil <mail@rushabh.dev>
2026-06-13 22:17:52 +0530

    `refactor: remove nested ternaries from padstack helpers`

--- lib/utils/create-and-add-padstack-for-pcb-smtpad.ts
+++ lib/utils/create-and-add-padstack-for-pcb-smtpad.ts
@@ -17,47 +17,63 @@ export function createAndAddPadstackFromPcbSmtPad(
   const isCircle = pad.shape === "circle"
   const isPolygon = pad.shape === "polygon"
   const polygonData = isPolygon ? getPolygonSmtPadData(pad) : undefined
+
+  let shape: PadstackNameArgs["shape"] = "rect"
+  let outerDiameter: number | undefined
+  let holeDiameter: number | undefined
+  let width: number | undefined
+  let height: number | undefined
+  let customDescriptor: string | undefined
+
+  if (isCircle) {
+    shape = "circle"
+    outerDiameter = pad.radius * 1000 * 2 // Radius to diameter
+    holeDiameter = pad.radius * 1000 * 2 // Radius to diameter
+  } else if (isPolygon) {
+    shape = "polygon"
+    width = polygonData?.widthUm
+    height = polygonData?.heightUm
+    customDescriptor = `${polygonData?.widthUm}x${polygonData?.heightUm}_${polygonData?.relativePointsUm.join("_")}`
+  } else {
+    width = (pad as { width: number }).width * 1000
+    height = (pad as { height: number }).height * 1000
+  }
+
   const padstackParams: PadstackNameArgs = {
-    shape: isCircle ? "circle" : isPolygon ? "polygon" : "rect",
-    outerDiameter: isCircle ? pad.radius * 1000 * 2 : undefined, // Radius to diameter
-    holeDiameter: isCircle ? pad.radius * 1000 * 2 : undefined, // Radius to diameter
-    width: isCircle
-      ? undefined
-      : isPolygon
-        ? polygonData?.widthUm
-        : (pad as { width: number }).width * 1000,
-    height: isCircle
-      ? undefined
-      : isPolygon
-        ? polygonData?.heightUm
-        : (pad as { height: number }).height * 1000,
+    shape,
+    outerDiameter,
+    holeDiameter,
+    width,
+    height,
     layer: pad.layer as PcbSmtPad["layer"],
-    customDescriptor: isPolygon
-      ? `${polygonData?.widthUm}x${polygonData?.heightUm}_${polygonData?.relativePointsUm.join("_")}`
-      : undefined,
+    customDescriptor,
   }
 
   const padstackName = getPadstackName(padstackParams)
 
   if (!processedPadstacks.has(padstackName)) {
-    const padstack: Padstack = isCircle
-      ? createCircularPadstack(
-          padstackName,
-          padstackParams.outerDiameter!,
-          padstackParams.holeDiameter!,
-        )
-      : isPolygon
-        ? createPolygonPadstack(
-            padstackName,
-            polygonData!.relativePointsUm,
-            pad.layer,
-          )
-        : createRectangularPadstack(
-            padstackName,
-            padstackParams.width!,
-            padstackParams.height!,
-            pad.layer,
-          )
+    let padstack: Padstack
+
+    if (isCircle) {
+      padstack = createCircularPadstack(
+        padstackName,
+        padstackParams.outerDiameter!,
+        padstackParams.holeDiameter!,
+      )
+    } else if (isPolygon) {
+      padstack = createPolygonPadstack(
+        padstackName,
+        polygonData!.relativePointsUm,
+        pad.layer,
+      )
+    } else {
+      padstack = createRectangularPadstack(
+        padstackName,
+        padstackParams.width!,
+        padstackParams.height!,
+        pad.layer,
+      )
+    }
 
     pcb.library.padstacks.push(padstack)
     processedPadstacks.add(padstackName)
